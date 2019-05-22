72032af7db7c7ee275db15cc2ed9c51da4f5f775
yahait-2 <taeyang916@naver.com>
2019-05-22 14:08:23 +0900

    rbpi

---
+++ relaycontrol/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render 
+from django.http import HttpResponse 
+# GPIO port numbers 
+import RPi.GPIO as GPIO
+import Adafruit_HTU21D.HTU21D as HTU21D
+import time 
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(21, GPIO.OUT)
+sensor = HTU21D.HTU21D()
+
+
+
+def relaycontroller(request): 
+    if 'on' in request.POST: 
+        GPIO.setup(20, GPIO.OUT)
+        time.sleep(1)
+        GPIO.output(21, 1)
+        time.sleep(1)
+        GPIO.output(21, 0)
+        time.sleep(1)
+        print('on')
+        print ('Temp = {0:0.2f} *C'.format(sensor.read_temperature()))
+        print ('Humidity  = {0:0.2f} %'.format(sensor.read_humidity()))
+        print ('Dew Point = {0:0.2f} *C'.format(sensor.read_dewpoint()))
+    elif 'off' in request.POST: 
+       GPIO.cleanup(20) 
+    return render(request,'control.html')
